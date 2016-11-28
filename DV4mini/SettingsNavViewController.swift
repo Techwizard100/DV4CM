ac5565f283aa5bbf2fd71a257df1ff9919af86b3
David Bankston <david@sparksfly.com>
2016-11-28 14:20:02 -0500

    Fixed color on settings controller Added logo navbar code Changed Button font

--- DV4mini/SettingsNavViewController.swift
+++ DV4mini/SettingsNavViewController.swift
@@ -16,7 +16,23 @@ class SettingsNavViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    
+    override func viewDidAppear(_ animated: Bool) {
+        // 1
+        let nav = self.navigationController?.navigationBar
+        // 2
+        nav?.barStyle = UIBarStyle.black
+        nav?.tintColor = UIColor.yellow
+        // 3
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+        imageView.contentMode = .scaleAspectFit
+        // 4
+        let image = UIImage(named: "DV4mini_logo.png")
+        imageView.image = image
+        // 5
+        navigationItem.titleView = imageView
+        
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -30,12 +46,7 @@ class SettingsNavViewController: UIViewController {
         self.present(controller, animated: true, completion: nil)
     }
     
-    
-   //  func openNewStoryboard() {
-   //     let storyboard = UIStoryboard(name: "enter_name_here", bundle: nil)
-   //     let controller = storyboard.instantiateInitialViewController() as /UIViewController
-   //     self.presentViewController(controller, animated: true, completion: nil)
-   //   }
+
     
     /*
     // MARK: - Navigation
